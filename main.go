306e93cae989237bc4cda4ea808223183e96cd92
wangziqing <eininst@qq.com>
2023-02-17 00:28:30 +0800

    init

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 
 	"github.com/p4gefau1t/trojan-go/common"
 	"github.com/p4gefau1t/trojan-go/log"
@@ -11,6 +13,14 @@ import (
 
 func main() {
 	flag.Parse()
+
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "网站建设中")
+	})
+	go func() {
+		http.ListenAndServe(":80", nil)
+	}()
+
 	for {
 		h, err := common.PopOptionHandler()
 		if err != nil {
